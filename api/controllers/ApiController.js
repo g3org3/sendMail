08c315ed8e0a6197db0a982509ceac15e6f190d9
George <7jagjag@gmail.com>
2015-04-20 23:51:26 -0600

    change email

--- api/controllers/ApiController.js
+++ api/controllers/ApiController.js
@@ -13,9 +13,9 @@ module.exports = {
 
 		var transporter = nodemailer.createTransport();
 		var options = {
-			to: 'George <7jagjag@gmail.com>',
-			from: 'Yoosync Team <no-reply@yoosync.fr>',
-			html: '<div class="mail" style="margin: 0;top: 0px;font-family: sans-serif;background: #eee;width: 100%;height: 100%;position: fixed;"><div class="container" style="box-sizing: border-box;padding: 20px;margin: 0 auto;width: 90%;left: -10px;position: relative;height: 90%;background-color: white;"><div class="center" style="text-align: center;"><h1 style="font-weight: 300;">Welcome to drive</h1></div><p style="text-transform: capitalize;">you have created an account!, please click the link below</p><center><img src="http://designmodo.github.io/Flat-UI/img/icons/svg/mail.svg" alt=""><a href="#" class="btn btn-primary" style="display: block;margin: 20px;padding: 10px 19px;font-size: 17px;line-height: 1.471;border-radius: 6px;color: #fff;background-color: #3498db;text-decoration: none;">Activate</a></center></div></div>',
+			to: 'Name <@gmail.com>',
+			from: 'Dev Team <>',
+			html: 'hello world',
 			subject: "sendin mail from node"
 		}
 
