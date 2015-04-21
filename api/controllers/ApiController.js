14c0e6481c33dd89c28d3da44f5cf4f573bc7dc4
George <7jagjag@gmail.com>
2015-04-20 23:17:06 -0600

    added nodemailer feature

--- api/controllers/ApiController.js
+++ api/controllers/ApiController.js
@@ -5,15 +5,32 @@
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
 
+var nodemailer = require('nodemailer');
+
 module.exports = {
 	
 	sendmail: function(req, res) {
+
+		var transporter = nodemailer.createTransport();
 		var options = {
 			to: '7jagjag@gmail.com',
 			from: 'george@node-server',
-			body: "hi man",
+			html: "<b>hi man</b>",
 			subject: "sendin mail from node"
 		}
+
+		transporter.sendMail(options, function(err, info){
+			if(err){
+				sails.log.error(err);
+				res.send({code:1, error:err});
+			}
+			else if(info){
+				sails.log.info(info);
+				res.json({code:0, info:info});
+			}
+			else
+				res.send("ok")
+		})
 	}
 };
 
